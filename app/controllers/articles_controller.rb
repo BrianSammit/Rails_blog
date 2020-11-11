434332b6358b0543a0d126d8ce0f778e5444c131
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-11 11:40:41 -0500

    add css file, index and show erb files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,13 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def show
+        @article = Article.find(params[:id])
+
+    end
+    
+    def find
+        @article = Article.find(params[:id])
+    end
 end
