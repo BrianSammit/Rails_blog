af5075c22702151aa55dba0f4456e0aebfee3f71
BrianSammit <briansammit.cruz@gmail.com>
2020-11-11 12:50:57 -0500

    Add methods delete and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,4 +22,20 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
 end
